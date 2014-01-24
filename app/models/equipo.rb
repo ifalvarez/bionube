035c2d7f66f8b22f6597314ffba962d69418d1d0
Ivan Alvarez <ivan.alvarez.r@gmail.com>
2014-01-24 15:06:08 -0500

    gear image for missing

--- app/models/equipo.rb
+++ app/models/equipo.rb
@@ -1,7 +1,5 @@
 class Equipo < ActiveRecord::Base
   belongs_to :user
   
-  has_attached_file :avatar, styles: {
-    medium: '300x300>'
-  }
+  has_attached_file :avatar, styles: {medium: '300x300>'}, :default_url => ":style/gear_orange.png"
 end
