9551ac564cdcfa81f46073799b069a0a553e22ac
Ivan Alvarez <ivan.alvarez.r@gmail.com>
2014-02-19 22:01:03 -0500

    added paperclip attackment mime validator

--- app/models/equipo.rb
+++ app/models/equipo.rb
@@ -5,4 +5,5 @@ class Equipo < ActiveRecord::Base
   has_many :ordenes
   
   has_attached_file :avatar, styles: {medium: '300x300>'}, :default_url => ":style/gear_orange.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
