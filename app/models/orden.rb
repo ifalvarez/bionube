f52c8758fae4fc372a8e218686ec275c1bf24d78
Ivan Alvarez <ivan.alvarez.r@gmail.com>
2014-04-20 13:48:47 -0500

    multiple references to user in ordenes working

--- app/models/orden.rb
+++ app/models/orden.rb
@@ -1,4 +1,6 @@
 class Orden < ActiveRecord::Base
   acts_as_commentable
   belongs_to :equipo
+  belongs_to :solicitante, :foreign_key => "solicitante_id", :class_name => "User"
+  belongs_to :tecnico, :foreign_key => "tecnico_id", :class_name => "User"
 end
